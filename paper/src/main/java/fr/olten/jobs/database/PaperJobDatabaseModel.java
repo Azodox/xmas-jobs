f866406317e67785daea5513f52e7c0876b110a1
Azodox_ <likemoi99@gmail.com>
2022-08-03 03:24:58 +0200

    Created PaperJobDatabaseModel that represents a job on Paper platform

---
+++ paper/src/main/java/fr/olten/jobs/database/PaperJobDatabaseModel.java
@@ -0,0 +1,13 @@
+package fr.olten.jobs.database;
+
+/**
+ * @author Azodox_ (Luke)
+ * 23/7/2022.
+ */
+
+public class PaperJobDatabaseModel extends JobDatabaseModel {
+
+    public PaperJobDatabaseModel(int id, String name, String description, String icon, String prefix, int powerId) {
+        super(id, name, description, icon, prefix, powerId);
+    }
+}
