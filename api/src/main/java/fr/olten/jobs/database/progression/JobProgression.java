50734559d3fe6842e67b3fc261e447b622c43d3c
Azodox_ <likemoi99@gmail.com>
2022-08-03 03:22:38 +0200

    Created JobProgression which represents a progression on a job

---
+++ api/src/main/java/fr/olten/jobs/database/progression/JobProgression.java
@@ -0,0 +1,29 @@
+package fr.olten.jobs.database.progression;
+
+import dev.morphia.annotations.Entity;
+import dev.morphia.annotations.Id;
+import dev.morphia.annotations.Property;
+import lombok.Getter;
+import org.bson.types.ObjectId;
+
+/**
+ * @author Azodox_ (Luke)
+ * 28/7/2022.
+ */
+
+@Entity(value = "progression", discriminator = "progression")
+public class JobProgression {
+
+    @Id
+    private ObjectId id;
+    @Property
+    private @Getter
+    final int jobId;
+    @Property
+    private @Getter final double xp;
+
+    public JobProgression(int jobId, double xp) {
+        this.jobId = jobId;
+        this.xp = xp;
+    }
+}
