43d0d62275d6b3fd83e43ca08d826dc9f49a3fbb
Azodox_ <likemoi99@gmail.com>
2022-08-11 02:50:20 +0200

    Added a list that contains multipliers + getter that calculates earned xp with multiplier and renamed earnedGeneralXP to earnedGlobalXP

--- api/src/main/java/fr/olten/jobs/database/mining/MinedBlockXP.java
+++ api/src/main/java/fr/olten/jobs/database/mining/MinedBlockXP.java
@@ -4,6 +4,8 @@ import dev.morphia.annotations.*;
 import lombok.Getter;
 import org.bson.types.ObjectId;
 
+import java.util.List;
+
 /**
  * @author Azodox_ (Luke)
  * 4/8/2022.
@@ -20,15 +22,36 @@ public class MinedBlockXP {
 
     @Property("minecraft_tag")
     private @Getter final String minecraftTag;
+    private @Getter final List<Float> levelMultipliers;
 
-    private @Getter final double earnedJobXP;
-    private @Getter final double earnedGeneralXP;
-    private @Getter final double earnedFlc;
+    private final double earnedJobXP;
+    private final double earnedGlobalXP;
+    private final double earnedFlc;
 
-    public MinedBlockXP(String minecraftTag, double earnedJobXP, double earnedGeneralXP, double earnedFlc) {
+    public MinedBlockXP(String minecraftTag, List<Float> levelMultipliers, double earnedJobXP, double earnedGlobalXP, double earnedFlc) {
         this.minecraftTag = minecraftTag;
+        this.levelMultipliers = levelMultipliers;
         this.earnedJobXP = earnedJobXP;
-        this.earnedGeneralXP = earnedGeneralXP;
+        this.earnedGlobalXP = earnedGlobalXP;
         this.earnedFlc = earnedFlc;
+    }
+
+    public double getEarnedJobXP(int level) {
+        return levelMultipliers.size() <= level ?
+                levelMultipliers.get(levelMultipliers.size() - 1) * earnedJobXP :
+                levelMultipliers.get(level) * earnedJobXP;
+    }
+
+    public double getEarnedGlobalXP(int level) {
+        return levelMultipliers.size() <= level ?
+                levelMultipliers.get(levelMultipliers.size() - 1) * earnedGlobalXP :
+                levelMultipliers.get(level) * earnedGlobalXP;
+
+    }
+
+    public double getEarnedFlc(int level) {
+        return levelMultipliers.size() <= level ?
+                levelMultipliers.get(levelMultipliers.size() - 1) * earnedFlc :
+                levelMultipliers.get(level) * earnedFlc;
     }
 }
